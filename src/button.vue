a25a3af66783b82b6b00765dcaaff43516af3e82
ZhiqiangYu <739291773@qq.com>
2018-08-10 17:51:30 +0800

    添加icon.vue

--- src/button.vue
+++ src/button.vue
@@ -1,8 +1,6 @@
 <template>
     <button class='g-button' :class="[`icon-${iconPosition}`]">
-        <svg v-if="icon" class="icon">
-            <use :xlink:href="`#i-${icon}`"></use>
-        </svg>
+        <g-icon v-if="icon" name="settings"></g-icon>
         <div class="content">
             <slot></slot>
         </div>
@@ -10,7 +8,20 @@
 </template>
 <script>
     export default{
-        props:['icon','iconPosition']
+        props:{
+            icon:{},
+            iconPosition:{
+                type:String,
+                default:'left',
+                validator(val){
+                    if(val !== 'left' && val !== 'right'){
+                        return false;
+                    }else{
+                        return true;
+                    }
+                }
+            }
+        }
     }
 </script>
 <style lang="scss">
---
+++ src/icon.vue
@@ -0,0 +1,21 @@
+<template>
+    <svg class="g-icon">
+        <use :xlink:href="`#i-${name}`"></use>
+    </svg>
+</template>
+
+<script>
+export default {
+    props:{
+        name:{}
+    }
+}
+</script>
+
+<style>
+    .g-icon {
+        width: 1em;
+        height: 1em;
+        margin-top: .2em;
+    }
+</style>
