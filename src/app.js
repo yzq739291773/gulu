02f7374fdf53c3a37a77d0e2470aa1d5651fa730
ZhiqiangYu <739291773@qq.com>
2018-08-11 12:09:42 +0800

    使用chai-spies监听第五个用例的回调函数

--- src/app.js
+++ src/app.js
@@ -17,7 +17,9 @@ new Vue({
 
 // 单元测试
 import chai from 'chai'
+import spies from 'chai-spies'
 const expect = chai.expect
+chai.use(spies)
 
 {
     const Constructor = Vue.extend(Button)
@@ -98,9 +100,9 @@ const expect = chai.expect
         }
     })
     vm.$mount()
-    vm.$on('click', function() {
-        expect(1).to.eq(1)
-    })
+    let spy = chai.spy(function() {})
+    vm.$on('click', spy)
     let button = vm.$el
     button.click()
+    expect(spy).to.have.been.called()
 }
