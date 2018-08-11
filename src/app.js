d14d6ee41206db62937a4193a5e618747f66bd7f
ZhiqiangYu <739291773@qq.com>
2018-08-11 22:33:38 +0800

    添加karma做自动化测试

--- src/app.js
+++ src/app.js
@@ -12,6 +12,7 @@ new Vue({
         loading1: false,
         loading2: false,
         loading3: false
+
     }
 })
 
--- src/button.vue
+++ src/button.vue
@@ -8,7 +8,11 @@
     </button>
 </template>
 <script>
+    import Icon from './icon.vue';
     export default{
+        components:{
+            'g-icon':Icon
+        },
         props:{
             icon:{},
             loading:{
