b8c69b29fe2a72c6452080e39c97c2b02c7e5979
ZhiqiangYu <739291773@qq.com>
2018-08-14 14:41:44 +0800

    row/col组件的雏形

---
+++ src/col.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="col" :class="[`col-${span}`]">
+        <slot></slot>
+    </div>
+</template>
+
+<script>
+export default {
+    name:'GuluCol',
+    props:{
+        span:{
+            type:[Number, String]
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped >
+    .col {
+            height: 50px;
+            width: 100%;
+            background-color: gray;
+            border: 1px solid red;
+
+            $class: col-;
+            @for $n from 1 through 24 {
+                &.#{$class}#{$n}{
+                    width: ($n / 24) * 100%;
+                }
+            }
+        }
+</style>
---
+++ src/row.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="row">
+        <slot></slot>
+    </div>
+</template>
+
+<script>
+export default {
+    name:'GuluRow'
+}
+</script>
+
+<style scoped lang="scss">
+    .row{
+        display: inline-flex;
+        flex-wrap: nowrap;
+        width: 100%;
+    }
+</style>
