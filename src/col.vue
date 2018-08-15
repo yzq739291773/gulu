9e8bc426fa2ec7fccb0cb5e51ba879766e784d66
ZhiqiangYu <739291773@qq.com>
2018-08-15 14:36:41 +0800

    响应式完成

--- src/col.vue
+++ src/col.vue
@@ -28,10 +28,6 @@ export default {
         offset:{
             type:[Number, String]
         },
-        phone:{
-            type:Object,
-            validator
-        },
         ipad:{
             type:Object,
             validator
@@ -64,12 +60,11 @@ export default {
             }
         },
         colClass(){
-            let {span, offset, phone, ipad, narrowPc, pc, widePc} = this
+            let {span, offset, ipad, narrowPc, pc, widePc} = this
             let phoneClass = []
             return [
                 span && `col-${span}`, 
                 offset &&  `offset-${offset}`,
-                ...(phone ? [`col-phone-${phone.span}`] : []),
                 ...(ipad ? [`col-ipad-${ipad.span}`] : []),
                 ...(narrowPc ? [`col-narrow-pc-${narrowPc.span}`] : []),
                 ...(pc ? [`col-pc-${pc.span}`] : []),
@@ -98,22 +93,7 @@ export default {
                     margin-left: ($n / 24) * 100%;
                 }
             }
-
-            @media screen and (max-width: 576px) {
-                $class-prefix: col-phone-;
-                @for $n from 1 through 24 {
-                    &.#{$class-prefix}#{$n}{
-                        width: ($n / 24) * 100%;
-                    }
-                }
-                $class-prefix: offset-phone-;
-                @for $n from 1 through 24 {
-                    &.#{$class-prefix}#{$n}{
-                        margin-left: ($n / 24) * 100%;
-                    }
-                }
-            }
-            @media screen and (min-width: 577px) and (max-width: 768px) {
+            @media screen and (min-width: 577px){
                 $class-prefix: col-ipad-;
                 @for $n from 1 through 24 {
                     &.#{$class-prefix}#{$n}{
@@ -127,7 +107,7 @@ export default {
                     }
                 }
             }
-            @media screen and (min-width: 769px) and (max-width: 992px) {
+            @media screen and (min-width: 769px){
                 $class-prefix: col-narrow-pc-;
                 @for $n from 1 through 24 {
                     &.#{$class-prefix}#{$n}{
@@ -141,7 +121,7 @@ export default {
                     }
                 }
             }
-            @media screen and (min-width: 993px) and (max-width: 1200px) {
+            @media screen and (min-width: 993px){
                 $class-prefix: col-pc-;
                 @for $n from 1 through 24 {
                     &.#{$class-prefix}#{$n}{
