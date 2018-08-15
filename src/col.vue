f2de58993609b84467559ff49c684aedd2192a1b
ZhiqiangYu <739291773@qq.com>
2018-08-15 10:12:41 +0800

    重构栅格组件

--- src/col.vue
+++ src/col.vue
@@ -1,6 +1,6 @@
 <template>
-    <div class="col" :class="[span && `col-${span}`, offset &&  `offset-${offset}`]"
-    :style="{paddingLeft:gutter/2+'px', paddingRight:gutter/2+'px'}">
+    <div class="col" :class="colClass" :style="colStyle">
+        <!-- 这个div是为了更好的理解，真正的代码不应该有这个div -->
         <div style="border:1px solid green;height:100%;">
             <slot></slot>
         </div>
@@ -22,7 +22,22 @@ export default {
     },
     data(){
         return{
-                gutter:0
+            gutter:0,
+            
+        }
+    },
+    computed:{
+        colStyle(){
+            return{
+                paddingLeft : this.gutter/2+'px', 
+                paddingRight : this.gutter/2+'px'
+            }
+        },
+        colClass(){
+            return [
+                this.span && `col-${this.span}`, 
+                this.offset &&  `offset-${this.offset}`
+            ]
         }
     }
 }
