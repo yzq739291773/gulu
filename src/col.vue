1b59d4fb90095b75a8d2374f937caf63f38a7725
ZhiqiangYu <739291773@qq.com>
2018-08-15 09:48:30 +0800

    基本实现栅格布局的功能

--- src/col.vue
+++ src/col.vue
@@ -1,6 +1,10 @@
 <template>
-    <div class="col" :class="[`col-${span}`]">
-        <slot></slot>
+    <div class="col" :class="[span && `col-${span}`, offset &&  `offset-${offset}`]"
+    :style="{paddingLeft:gutter/2+'px', paddingRight:gutter/2+'px'}">
+        <div style="border:1px solid green;height:100%;">
+            <slot></slot>
+        </div>
+        
     </div>
 </template>
 
@@ -10,6 +14,15 @@ export default {
     props:{
         span:{
             type:[Number, String]
+        },
+        offset:{
+            type:[Number, String]
+        },
+        
+    },
+    data(){
+        return{
+                gutter:0
         }
     }
 }
@@ -17,15 +30,22 @@ export default {
 
 <style lang="scss" scoped >
     .col {
+            // padding: 0 10px;
             height: 50px;
             width: 100%;
-            background-color: gray;
-            border: 1px solid red;
+            // background-color: gray;
+            // border: 1px solid red;
 
-            $class: col-;
+            $class-prefix: col-;
             @for $n from 1 through 24 {
-                &.#{$class}#{$n}{
+                &.#{$class-prefix}#{$n}{
                     width: ($n / 24) * 100%;
+                }
+            }
+            $class-prefix: offset-;
+            @for $n from 1 through 24 {
+                &.#{$class-prefix}#{$n}{
+                    margin-left: ($n / 24) * 100%;
                 }
             }
         }
--- src/row.vue
+++ src/row.vue
@@ -1,19 +1,31 @@
 <template>
-    <div class="row">
+    <div class="row" :style="{marginLeft:-gutter/2 + 'px', marginRight:-gutter/2 + 'px'}">
         <slot></slot>
     </div>
 </template>
 
 <script>
 export default {
-    name:'GuluRow'
+    name:'GuluRow',
+    props:{
+        gutter:{
+            type:[Number,String]
+        }
+    },
+    mounted(){
+        this.$children.forEach((vm)=>{
+            vm.gutter = this.gutter
+        })
+    }
 }
 </script>
 
 <style scoped lang="scss">
     .row{
-        display: inline-flex;
+        // 注意：栅格布局gutter的关键就是col设置左右padding，然后row设置左右负margin,让row分别向左右延伸，
+        // 但是设置负margin会延伸的前提是，该元素不能设置有width属性，width 100%都不行
+        // margin: 0 -10px;
+        display: flex;
         flex-wrap: nowrap;
-        width: 100%;
     }
 </style>
