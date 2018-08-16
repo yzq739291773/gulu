b5f77e7afe6f34b3136d40b3f9faba7f8922c16e
ZhiqiangYu <739291773@qq.com>
2018-08-16 08:52:50 +0800

    默认布局完成

--- src/layout.vue
+++ src/layout.vue
@@ -1,20 +1,38 @@
 <template>
-    <div class="layout">
+    <div class="layout" :class="layoutClass">
         <slot></slot>
     </div>
 </template>
 
 <script>
 export default {
-
+    name:'GuluLayout',
+    data(){
+        return{
+            layoutClass:{
+                hasSider:false
+            }
+        }
+    },
+    mounted(){
+        this.$children.forEach((vm)=>{
+            if(vm.$options.name === 'GuluSider'){
+                this.layoutClass.hasSider = true;
+            }
+        })
+    }
 }
 </script>
 
 <style scoped lang="scss">
     .layout{
+        flex-grow: 1;
         border: 1px solid red;
         display: flex;
         flex-direction: column;
+        &.hasSider{
+            flex-direction: row;
+        }
     }
 
 </style>
--- src/sider.vue
+++ src/sider.vue
@@ -1,10 +1,12 @@
 <template>
-    
+    <div class="sider">
+        <slot></slot>
+    </div>
 </template>
 
 <script>
 export default {
-
+    name:'GuluSider'
 }
 </script>
 
