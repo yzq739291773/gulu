17f65102c6568ec7918bda8a021e90af596a9506
ZhiqiangYu <739291773@qq.com>
2018-08-16 17:11:44 +0800

    添加 toast 的autoClose的测试用例

--- test/toast.test.js
+++ test/toast.test.js
@@ -10,22 +10,22 @@ describe('Toast', () => {
         expect(Toast).to.be.ok
     })
 
-    // describe('属性', () => {
-    //     it('接收 autoClose', (done) => {
-    //         let div = document.createElement('div')
-    //         document.body.appendChild(div)
-    //         const Constructor = Vue.extend(Toast)
-    //         const vm = new Constructor({
-    //             propsData: {
-    //                 autoClose: true,
-    //                 autoCloseDelay: 1000
-    //             }
-    //         }).$mount(div)
-    //         setTimeout(() => {
-    //             expect(document.body.contains(vm.$el)).to.eq(false)
-    //             done()
-    //         }, 1500)
-    //     })
+    describe('属性', () => {
+        it('接收 autoClose', (done) => {
+            let div = document.createElement('div')
+            document.body.appendChild(div)
+            const Constructor = Vue.extend(Toast)
+            const vm = new Constructor({
+                propsData: {
+                    autoClose: true,
+                    autoCloseDelay: 1
+                }
+            }).$mount(div)
+            setTimeout(() => {
+                expect(document.body.contains(vm.$el)).to.eq(false)
+                done()
+            }, 1500)
+        })
 
-    // })
+    })
 })
