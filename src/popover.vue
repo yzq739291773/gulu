c362d395a37b82a71ad184756593a8e708610fac
ZhiqiangYu <739291773@qq.com>
2018-08-23 17:27:32 +0800

    解决阻止冒泡造成的bug
    通过判断具体点击的target来判断

--- src/popover.vue
+++ src/popover.vue
@@ -1,6 +1,6 @@
 <template>
-    <div class="popover" @click.stop="onClick">
-        <div ref="contentWrapper" class="content-wrapper" v-if="visible" @click.stop="">
+    <div class="popover" @click="onClick">
+        <div ref="contentWrapper" class="content-wrapper" v-if="visible" @click="">
             <slot name="content"></slot>
         </div>
         <div ref="triggerWrapper">
@@ -21,28 +21,37 @@ export default {
         
     },
     methods:{
-        onClick(){
-            this.visible = !this.visible
-            console.log('切换visible')
-            if(this.visible == true){
-                this.$nextTick(()=>{
-                    document.body.appendChild(this.$refs.contentWrapper)
+        onClick(e){
+            if(this.$refs.triggerWrapper.contains(e.target)){
+                this.visible = !this.visible
+                console.log('切换visible')
+                if(this.visible == true){
+                    this.$nextTick(()=>{
+                        document.body.appendChild(this.$refs.contentWrapper)
 
-                    let {left, top, width, height} = this.$refs.triggerWrapper.getBoundingClientRect()
-                    // pageXOffset是scrollX的别名，前者无兼容性，后者有
-                    this.$refs.contentWrapper.style.left = left + window.pageXOffset+ 'px'
-                    this.$refs.contentWrapper.style.top = top + window.pageYOffset+'px'
+                        let {left, top, width, height} = this.$refs.triggerWrapper.getBoundingClientRect()
+                        // pageXOffset是scrollX的别名，前者无兼容性，后者有
+                        this.$refs.contentWrapper.style.left = left + window.pageXOffset+ 'px'
+                        this.$refs.contentWrapper.style.top = top + window.pageYOffset+'px'
 
-                    let eventHandler = ()=>{
-                        console.log('关闭popover')
-                        this.visible = false
-                        console.log('移除监听函数')
-                        document.removeEventListener('click',eventHandler)
-                    }
-                    console.log('绑定监听函数')
-                    document.addEventListener('click',eventHandler)
-                })
+                        let eventHandler = (event)=>{
+                            if(this.$refs.contentWrapper.contains(event.target)){
+
+                            }else{
+                                console.log('关闭popover')
+                                this.visible = false
+                                console.log('移除监听函数')
+                                document.removeEventListener('click',eventHandler)
+                            }
+                        }
+                        console.log('绑定监听函数')
+                        document.addEventListener('click',eventHandler)
+                    })
+                }
+            }else{
+
             }
+            
         }
     }
 }
