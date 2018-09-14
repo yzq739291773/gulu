5f9123a35f9e8ef129339f55273599ef028b73b1
yzq <739291773@qq.com>
2018-09-14 15:58:43 +0800

    级联错误的实现方式

--- src/cascader.vue
+++ src/cascader.vue
@@ -1,11 +1,24 @@
 <template>
     <div class="cascader">
-        <div class="trigger">
-            <slot></slot>
+        <div class="trigger" @click="popoverVisible = !popoverVisible">
         </div>
-        <div class="popover">
-            <div v-for="item in source">
-                <cascader-item :sourceItem="item"></cascader-item>
+        <div class="popover" v-if="popoverVisible">
+            <div class="level1">
+                <div class="label" v-for="item1 in source" style="border:1px solid red"
+                @click="level1Selected = item1">
+                    {{item1.name}}
+                </div>
+            </div>
+            <div class="level2">
+                <div class="label" v-for="item2 in level2Items"
+                @click="level2Selected = item2">
+                    {{item2.name}}
+                </div>
+            </div>
+            <div class="level3">
+                <div class="label" v-for="item3 in level3Items">
+                    {{item3.name}}
+                </div>
             </div>
         </div>
     </div>
@@ -22,6 +35,29 @@ export default {
         source:{
             type:Array
         }
+    },
+    data(){
+        return{
+            popoverVisible:false,
+            level1Selected:null,
+            level2Selected:null
+        }
+    },
+    computed:{
+        level2Items(){
+            if(this.level1Selected){
+                return this.level1Selected.children
+            }else{
+                return []
+            }
+        },
+        level3Items(){
+            if(this.level2Selected){
+                return this.level2Selected.children
+            }else{
+                return []
+            }
+        }
     }
 
 }
@@ -29,5 +65,20 @@ export default {
 
 <style lang="scss" scoped>
     @import 'var.scss';
-
+    .cascader{
+        .trigger{
+            border: 1px solid red;
+            height: 32px;
+            width: 100px;
+        }
+        .popover{
+            border: 2px solid green;
+            height: 200px;
+            width: 80px;
+            display: flex;
+            .label{
+                white-space: nowrap;
+            }
+        }
+    }
 </style>
