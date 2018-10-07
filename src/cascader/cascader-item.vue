38fb2ad05378f9a03adeaae327e5559b9cd5e838
yzq <739291773@qq.com>
2018-10-07 14:02:36 +0800

    级联选择器

--- src/cascader/cascader-item.vue
+++ src/cascader/cascader-item.vue
@@ -1,108 +1,122 @@
 <template>
-    <div class="cascaderItem" :style="{height:height}">
-        <div class="left">
-            <div class="label" v-for="item in items" @click="onClickTab(item)">
-                {{item.name}}
-                <g-icon class="icon" v-if="item.children" name="right"></g-icon>
-            </div>
-        </div>
-        <div class="right"  v-if="rightItems">
-            <gulu-cascader-item 
-                ref="right"
-                :items="rightItems" 
-                :height="height"
-                :selected="selected"
-                @update:selected="onUpdateSelected"
-                :level="level + 1"></gulu-cascader-item>
-        </div>
+  <div class="cascaderItem" :style="{height: height}">
+    <div class="left">
+      <div class="label" v-for="item in items" @click="onClickLabel(item)">
+        <span class="name">{{item.name}}</span>
+        <span class="icons">
+          <template v-if="item.name === loadingItem.name">
+            <icon class="loading" name="loading"></icon>
+          </template>
+          <template v-else>
+            <icon class="next" v-if="rightArrowVisible(item)" name="right"></icon>
+          </template>
+        </span>
+      </div>
     </div>
+    <div class="right" v-if="rightItems">
+      <gulu-cascader-items ref="right" :items="rightItems" :height="height"
+        :loading-item="loadingItem"
+        :load-data="loadData"
+        :level="level+1" :selected="selected" @update:selected="onUpdateSelected"></gulu-cascader-items>
+    </div>
+  </div>
 </template>
 
 <script>
-import Icon from '../icon/icon.vue';
-export default {
-    name:'GuluCascaderItem',
-    components:{
-        'g-icon':Icon
+  import Icon from '../icon'
+  export default {
+    name: "GuluCascaderItems",
+    components: {Icon},
+    props: {
+      items: {
+        type: Array
+      },
+      height: {
+        type: String
+      },
+      loadingItem: {
+        type: Object,
+        default: () => ({})
+      },
+      selected: {
+        type: Array,
+        default: () => []
+      },
+      loadData: {
+        type: Function
+      },
+      level: {
+        type: Number,
+        default: 0
+      }
     },
-    props:{
-        items:{
-            type:Array
-        },
-        height:{
-            type:String
-        },
-        selected:{
-            type:Array,
-            default:()=>{
-                return []
-            }
-        },
-        level:{
-            type:Number,
-            default:0
+    computed: {
+      rightItems () {
+        if (this.selected[this.level]) {
+          let selected = this.items.filter((item) => item.name === this.selected[this.level].name)
+          if (selected && selected[0].children && selected[0].children.length > 0) {
+            return selected[0].children
+          }
         }
+      },
     },
-    data(){
-        return{
-        }
+    mounted () {
     },
-    computed:{
-        rightItems(){
-            if(this.selected[this.level]){
-                let selected = this.items.filter(item =>{
-                    item.name === this.selected[this.level].name
-                })
-                if(selected && selected[0].children &&selected[0].children.length>0){
-                    return selected[0].children
-                }
-            }
-            // let currentSelected = this.selected[this.level]
-            // if(currentSelected && currentSelected.children){
-            //     return currentSelected.children
-            // }else{
-            //     return null
-            // }
-        }
-    },
-    methods:{
-        onClickTab(item){
-            // 1.子组件不能直接修改props
-            // 2.数组这种数据直接修改后，vue是捕捉不到他的修改的，自然不会渲染dom,因此需要拷贝
-            let copy = JSON.parse(JSON.stringify(this.selected))
-            copy[this.level] = item
-            copy.splice(this.level+1)
-            this.$emit('update:selected',copy)
-        },
-        onUpdateSelected(newSelected){
-            this.$emit('update:selected',newSelected)
-        }
+    methods: {
+      rightArrowVisible (item) {
+        return this.loadData ? !item.isLeaf : item.children
+      },
+      onClickLabel (item) {
+        let copy = JSON.parse(JSON.stringify(this.selected))
+        copy[this.level] = item
+        copy.splice(this.level + 1) // 一句话
+        this.$emit('update:selected', copy)
+      },
+      onUpdateSelected (newSelected) {
+        this.$emit('update:selected', newSelected)
+      }
     }
-}
+  }
 </script>
 
-<style lang="scss" scoped>
-    @import '../../styles/var';
-    .cascaderItem {
-        display: flex;
-        align-items: flex-start;
-        justify-content: flex-start;
-        height: 100px;
-        .left{
-            height: 100%;
-            padding-top: .3em;
-            overflow: auto;
-            .icon{
-                transform: scale(0.5)
-            }
-        }
-        .right{
-            
-            height: 100%;
-            border-left: 1px solid $border-color-light;
-        }
-        .label{
-            padding: .3em 1em;
-        }
+<style scoped lang="scss">
+  @import "var";
+  .cascaderItem {
+    display: flex;
+    align-items: flex-start;
+    justify-content: flex-start;
+    height: 100px;
+    .left {
+      height: 100%;
+      padding: .3em 0;
+      overflow: auto;
     }
+    .right {
+      height: 100%;
+      border-left: 1px solid $border-color-light;
+    }
+    .label {
+      padding: .5em 1em;
+      display: flex;
+      align-items: center;
+      cursor: pointer;
+      white-space: nowrap;
+      &:hover {
+        background: $grey;
+      }
+      > .name {
+        margin-right: 1em;
+        user-select: none;
+      }
+      .icons {
+        margin-left: auto;
+        .next {
+          transform: scale(0.5);
+        }
+        .loading {
+          animation: spin 2s infinite linear;
+        }
+      }
+    }
+  }
 </style>
